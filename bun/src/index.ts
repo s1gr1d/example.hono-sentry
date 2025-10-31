329185586dca8e9840140bd5ce9fdd5af54370ae
s1gr1d <32902192+s1gr1d@users.noreply.github.com>
2025-10-31 15:24:52 +0100

    add @sentry/bun

--- bun/src/index.ts
+++ bun/src/index.ts
@@ -1,9 +1,20 @@
 import { Hono } from "hono";
 
+import * as Sentry from "@sentry/bun";
+import api from "./api";
+
 const app = new Hono();
+
+Sentry.init({
+  dsn: import.meta.env.SENTRY_DSN,
+  tracesSampleRate: 1,
+  debug: true,
+});
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.route("/bun-api", api);
 
 export default app;
